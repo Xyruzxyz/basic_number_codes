9acd4bc84e08a751dfef8c4f32d41e8a47511e0f
Xyruzxyz <rhexyruz@gmail.com>
2025-03-16 03:15:44 +0800

    rewrite

--- Batch 2 Number code/7#Count_even_numbers_in_10_inputs.py
+++ Batch 2 Number code/7#Count_even_numbers_in_10_inputs.py
@@ -3,4 +3,4 @@ for i in range(10):
     num = int(input("Enter number: "))
     if num % 2 == 0:
         even_count += 1
-print("Even numbers count:", even_count)
+print("Even numbers count:", even_count)                     
---
+++ Batch 2 Number code/8#Print all odd numbers from 0 to 100 using while loop.py
@@ -0,0 +1,8 @@
+#Initialize
+num = 1 #start from 1 since 0 is not odd
+#repeat 100 times
+while num <= 100:
+    #print the number
+    print(num)
+    #increment the number by 2
+    num += 2
