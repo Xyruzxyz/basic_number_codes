67e6a699036ee35ebb54aef9b2b9eb34fadd1b57
Xyruzxyz <rhexyruz@gmail.com>
2025-03-16 03:18:06 +0800

    It works finally

--- Batch 2 Number code/7#Count_even_numbers_in_10_inputs.py
+++ Batch 2 Number code/7#Count_even_numbers_in_10_inputs.py
@@ -1,6 +1,12 @@
-even_count = 0
+# set count to 0
+even_number= 0 
+#repeat 10 times
 for i in range(10):
-    num = int(input("Enter number: "))
-    if num % 2 == 0:
-        even_count += 1
-print("Even numbers count:", even_count)                     
+    #get the input
+    num = int(input("Enter a number: "))
+    #check if the number is even
+    if num%2 == 0:
+        #increment the counter
+        even_number += 1
+        #print even count
+print("Even count: ", even_number)     
