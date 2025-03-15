df0eae58711802e3539b6ed4b18d58ba718c6119
Xyruzxyz <rhexyruz@gmail.com>
2025-03-15 23:43:11 +0800

    code 4 working

--- Batch 2 Number code/4#Print the quotient without decimal point.py
+++ Batch 2 Number code/4#Print the quotient without decimal point.py
@@ -1,3 +1,5 @@
 #Input 2 numbers
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
+#Print quotient without decimal point
+print("Quotient without decimal point: ", num1 // num2)
