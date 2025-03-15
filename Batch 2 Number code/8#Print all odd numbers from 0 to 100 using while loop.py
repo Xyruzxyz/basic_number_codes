2509d4d6bbcd329314597ac15936ce5bb7ae5c54
Xyruzxyz <rhexyruz@gmail.com>
2025-03-16 00:15:40 +0800

    Increment is right

---
+++ Batch 2 Number code/8#Print all odd numbers from 0 to 100 using while loop.py
@@ -0,0 +1,9 @@
+#Initialize
+num = 1 #start from 1 since 0 is not odd
+#repeat 100 times
+while num <= 100:
+    #print the number
+    print(num)
+    #increment the number by 2
+    num += 2
+print("done")
