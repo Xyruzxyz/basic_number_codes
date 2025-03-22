bc54d66b242b0e474d713a24096e930206cf43f8
Xyruzxyz <rhexyruz@gmail.com>
2025-03-22 15:21:32 +0800

    Update 8#odd_numbers.py
    simplify

--- Batch 1 Number code/8#odd_numbers.py
+++ Batch 1 Number code/8#odd_numbers.py
@@ -1,8 +1,9 @@
 odd_numbers = 0
 for i in range(10):
-    num = int(input(f"{i+1}.  Enter an odd number: "))
+    num = int(input("Enter an odd number: "))
     if num % 2 !=0: 
         odd_numbers += 1
+    print("Total is: ", odd_numbers)
     else:
         print("That was not an odd number.")
 print(f"You entered {odd_numbers} odd numbers.")
