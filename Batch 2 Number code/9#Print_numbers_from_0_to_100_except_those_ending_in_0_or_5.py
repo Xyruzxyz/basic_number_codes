e9b8cfbaa50b1f36286c70d6116e01a81362e108
Xyruzxyz <rhexyruz@gmail.com>
2025-03-22 16:00:06 +0800

    Update 9#Print_numbers_from_0_to_100_except_those_ending_in_0_or_5.py

--- Batch 2 Number code/9#Print_numbers_from_0_to_100_except_those_ending_in_0_or_5.py
+++ Batch 2 Number code/9#Print_numbers_from_0_to_100_except_those_ending_in_0_or_5.py
@@ -1,5 +1,5 @@
 #use for loop to print numbers from 0 to 100 except those ending in 0 or 5
 for num in range(101):
 #check if the number is not ending in 0 or 5
-    if num%10 != 0 and num%10 != 5:
+    if num%5 != 0:
         print(num)
